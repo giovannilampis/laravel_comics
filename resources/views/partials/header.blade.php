30f6749316f2131264d23eb51ae00a64186e4e6b
giovannilampis <giolampis@hotmail.com>
2023-06-12 09:34:07 +0200

    add list-unstyled bootstrap class header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,17 +1,20 @@
-<nav class="d-flex justify-content-between align-items-center">
-    <div class="logo-container">
-        <img src="#" alt="logo dc">
-    </div>
-    <ul class="text-uppercase m-0 d-flex justify-content-between align-items-center">
-        <li><a href="#">characters</a></li>
-        <li><a href="#">comics</a></li>
-        <li><a href="#">movies</a></li>
-        <li><a href="#">tv</a></li>
-        <li><a href="#">games</a></li>
-        <li><a href="#">collectibles</a></li>
-        <li><a href="#">videos</a></li>
-        <li><a href="#">fans</a></li>
-        <li><a href="#">news</a></li>
-        <li><a href="#">shop</a></li>
-    </ul>
-</nav>
+
+<header>
+    <nav class="d-flex justify-content-between align-items-center">
+        <div class="logo-container">
+            <img src="#" alt="logo_dc">
+        </div>
+        <ul class="text-uppercase m-0 d-flex justify-content-between align-items-center list-unstyled">
+            <li><a href="#">characters</a></li>
+            <li><a href="#">comics</a></li>
+            <li><a href="#">movies</a></li>
+            <li><a href="#">tv</a></li>
+            <li><a href="#">games</a></li>
+            <li><a href="#">collectibles</a></li>
+            <li><a href="#">videos</a></li>
+            <li><a href="#">fans</a></li>
+            <li><a href="#">news</a></li>
+            <li><a href="#">shop</a></li>
+        </ul>
+    </nav>
+</header>
