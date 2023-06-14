de4898ad508f4d0b64042dea46a4e62138db27b1
giovannilampis <giolampis@hotmail.com>
2023-06-14 05:05:15 +0200

    add footer content and basic style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,79 @@
-FOOTER
+<section>
+
+    <div class="top-footer">
+
+        <div class="top-footer-container">
+
+            <div class="d-flex flex-column">
+
+                <ul>
+                    <li class="text-white text-uppercase"><h5>DC COMICS</h5></li>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+
+                </ul>
+
+                <ul>
+                    <li class="text-white text-uppercase mt-4"><h5>shop</h5></li>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
+
+                </ul>
+
+            </div>
+
+            <ul>
+                <li><h5 class="text-white text-uppercase">dc</h5></li>
+                <li>Terms Of Use</li>
+                <li>Privacy policy (New)</li>
+                <li>Ad Choices</li>
+                <li>Advertising</li>
+                <li>Jobs</li>
+                <li>Subscriptions</li>
+                <li>Talent Workshoos</li>
+                <li>CPSC Certificates</li>
+                <li>Ratings</li>
+                <li>Shop Help</li>
+                <li>Contact Us</li>
+            </ul>
+
+            <ul>
+                <li><h5 class="text-white text-uppercase">SITES</h5></li>
+                <li>DC</li>
+                <li>MAD Magazine</li>
+                <li>DC Kids</li>
+                <li>DC Universe</li>
+                <li>DC Power Visa</li>
+            </ul>
+
+        </div>
+
+    </div>
+
+    <div class="bottom-footer">
+
+        <div class="bottom-footer-container d-flex justify-content-between align-items-center text-uppercase">
+
+            <a href="#" class="sign-up">sign-up now!</a>
+
+            <ul class="d-flex">
+
+                <li class="d-flex align-items-center"><a href="#">follow us</a></li>
+                <li><a href="#"><img src="/public/img/footer-facebook.png" alt="facebook logo link"></a></li>
+                <li><a href="#"><img src="/public/img/footer-twitter.png" alt="twitter logo link"></a></li>
+                <li><a href=""><img src="/public/img/footer-youtube.png" alt="youtube logo link"></a></li>
+                <li><a href="#"><img src="/public/img/footer-pinterest.png" alt="pinterest logo link"></a></li>
+                <li><a href="#"><img src="/public/img/footer-periscope.png" alt="periscope logo link"></a></li>
+
+            </ul>
+
+        </div>
+
+    </div>
+
+</section>
